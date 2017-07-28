c21928e8ddb291728dd3fe73cc01e353352cc670
Tom-Plummer <1thomas.plummer1@gmail.com>
2017-07-28 02:22:13 -0500

    Created a basic interface for IDraggable and IHasDraggableChildren.

---
+++ osu.Framework/Graphics/Containers/IDraggable.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using osu.Framework.Input;
+
+namespace osu.Framework.Graphics.Containers
+{
+    public interface IDraggable : IDrawable
+    {
+        Action<Drawable, InputState> DragStart { set; }
+
+        Action<Drawable, InputState> Drag { set; }
+
+        Action<Drawable, InputState> DragEnd { set; }
+    }
+}
---
+++ osu.Framework/Graphics/Containers/IHasDraggableChildren.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using osu.Framework.Input;
+
+namespace osu.Framework.Graphics.Containers
+{
+    public interface IHasDraggableChildren : IDrawable
+    {
+        void OnChildDragStart(Drawable child, InputState state);
+
+        void OnChildDrag(Drawable child, InputState state);
+
+        void OnChildDragEnd(Drawable child, InputState state);
+    }
+}
